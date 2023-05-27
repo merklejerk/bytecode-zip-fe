ebd3da8d0d189a3155d02e82aae4fb5f9c11ed32
Lawrence Forman <me@merklejerk.com>
2023-05-26 22:47:23 -0400

    dum dee dum

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+import "@picocss/pico";
+
+</script>
+
+<style lang="scss">
+    @font-face {
+        font-family: 'VT323';
+        src: url('/VT323-Regular.ttf') format('truetype');
+    }
+    @font-face {
+        font-family: '3270';
+        src: url('/3270-Regular.ttf') format('truetype');
+    }
+    @font-face {
+        font-family: 'IBM';
+        src: url('/IBM.ttf') format('truetype');
+    }
+    @font-face {
+        font-family: 'Atari';
+        src: url('/Atari.ttf') format('truetype');
+    }
+</style>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,71 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let bytecode: string | undefined;
+    let textInput: HTMLDivElement;
+
+    function pasteHandler(e: ClipboardEvent) {
+        bytecode = e.clipboardData?.getData("text")
+            .replaceAll(/\s+/g, '');
+        console.log('foo', bytecode);
+    }
+
+    function dropHandler(e: DragEvent) {
+        console.log(e.dataTransfer?.files);
+    }
+
+    function keyHandler(e: KeyboardEvent) {
+        console.log(e.code);
+        if (e.key === 'Escape') {
+            e.preventDefault();
+            e.stopPropagation();
+            bytecode = undefined;
+        }
+    }
+</script>
+<style lang="scss">
+    main {
+        height: 100vh;
+        padding: 0;
+        margin: 0;
+        background-color: rgb(82, 59, 82);
+        position: fixed;
+        font-family: 'Atari';
+        
+        .text-input {
+            color: rgb(108, 121, 112);
+            margin: 0;
+            padding: 0;
+            font-size: 3em;
+            caret-color: transparent;
+            border: 0;
+            white-space: break-spaces;
+            word-break: break-all;
+            overflow: hidden;
+            line-height: 1em;
+            max-height: 100%;
+            min-height: 100%;
+            user-select: none;
+
+            .caret {
+                animation: blink-animation 1s steps(2, start) infinite;
+            }
+        }
+    }
+
+    @keyframes blink-animation {
+        to {
+          visibility: hidden;
+        }
+      }
+</style>
+
+<svelte:document on:paste|preventDefault={pasteHandler} on:keyup={keyHandler} />
+<main>
+    <div
+        bind:this={textInput}
+        class="text-input"
+        on:scroll|stopPropagation|preventDefault={() => textInput.scrollTop = 0}
+        on:resize={() => textInput.scrollTop = 0}
+        on:drop|stopPropagation|preventDefault={dropHandler}
+        on:dragover|preventDefault|stopPropagation
+    >{#if bytecode}{ bytecode }{:else}<span class="caret">▊</span>{/if}</div>
+</main>
