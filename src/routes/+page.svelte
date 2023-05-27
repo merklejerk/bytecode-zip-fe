2ab5b6a47234a7d66fcf3d887a3a147a4b4093e7
Lawrence Forman <me@merklejerk.com>
2023-05-27 01:18:18 -0400

    work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,6 @@
     }
 
     function keyHandler(e: KeyboardEvent) {
-        console.log(e.code);
         if (e.key === 'Escape') {
             e.preventDefault();
             e.stopPropagation();
@@ -22,19 +21,22 @@
     }
 </script>
 <style lang="scss">
+    :root {
+        --console-text-color: rgb(108, 121, 112);
+    }
     main {
         height: 100vh;
         padding: 0;
         margin: 0;
-        background-color: rgb(82, 59, 82);
+        background-color: rgb(44, 30, 32);
         position: fixed;
         font-family: 'Atari';
         
         .text-input {
-            color: rgb(108, 121, 112);
+            color: var(--console-text-color);
             margin: 0;
             padding: 0;
-            font-size: 3em;
+            font-size: 2em;
             caret-color: transparent;
             border: 0;
             white-space: break-spaces;
@@ -46,16 +48,64 @@
             user-select: none;
 
             .caret {
+                display: inline-block;
+                background-color: var(--console-text-color);
+                width: 1ex;
+                height: 1.25ex;
+                margin-left: 0.25ex;
                 animation: blink-animation 1s steps(2, start) infinite;
             }
+        }
+
+        .crt-effect-1 {
+            --crt-glow-1: 196, 146, 194;
+
+            position: absolute;
+            inset: 0;
+            // background: url('/crt-effect-1.svg') center;
+            // background-size: 105% 105%;
+            // mix-blend-mode: multiply;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-evenly;
+            gap: 1em;
+            box-shadow: inset 0 0 min(10vh,10vw) rgba(27, 2, 10, 0.75);
+            
+            .crt-line {
+                width: 100%;
+                height: 0.1em;
+                box-shadow: 0 0 1em rgba(var(--crt-glow-1), 0.05);
+                background-color: rgba(var(--crt-glow-1), 0.0075);
+            }
+            .crt-scan-line {
+                animation: scan-line-animation 2s linear 0s infinite;
+                position: absolute;
+                width: 100%;
+                height: 10em;
+                background-image: linear-gradient(
+                    rgba(var(--crt-glow-1), 0) 0%,
+                    rgba(var(--crt-glow-1), 0.05) 85%,
+                    rgba(var(--crt-glow-1), 0.075) 99%,
+                    rgba(var(--crt-glow-1), 0.33) 100%
+                );
+            }
+        }
+    }
+    
+    @keyframes scan-line-animation {
+        from {
+            top: calc(0% - 10em);
+        }
+        to {
+            top: 100%;
         }
     }
 
     @keyframes blink-animation {
         to {
-          visibility: hidden;
+            visibility: hidden;
         }
-      }
+    }
 </style>
 
 <svelte:document on:paste|preventDefault={pasteHandler} on:keyup={keyHandler} />
@@ -67,5 +117,17 @@
         on:resize={() => textInput.scrollTop = 0}
         on:drop|stopPropagation|preventDefault={dropHandler}
         on:dragover|preventDefault|stopPropagation
-    >{#if bytecode}{ bytecode }{:else}<span class="caret">▊</span>{/if}</div>
+    >
+        {#if bytecode}
+            { bytecode }
+        {:else}
+            <span class="caret" />
+        {/if}
+    </div>
+    <div class="crt-effect-1">
+        {#each new Array(15) as n}
+            <div class="crt-line" />
+        {/each}
+        <div class="crt-scan-line" />
+    </div>
 </main>
